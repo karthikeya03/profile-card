53d0711581cd7acdbb3393da31232f08bc12ab85
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2024-07-12 00:41:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function loadMainContent() {
     document.getElementById('main-content').innerHTML = `
         <div class="div1">
             <div class="profile">
-                <img src="2026.png" alt="Profile Image">
+                <img src="E:\\HTML\\PROFILE 2\\2026.png" alt="Profile Image">
             </div>
         </div>
         <div class="div2">
@@ -11,7 +11,7 @@ function loadMainContent() {
         </div>
         <div class="social"> 
             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=saisatyakarthikeya@gmail.com" class="social-mail"><i class="fa-solid fa-envelope"></i></a>
-<a href="https://wa.me/+917013796123?text=Hello! I'm[Your%20Name],%20came%20across%20something%20interesting%20for%20you.%20Ping%20me%20back%20on%20WhatsApp!" class="social-call"><i class="fa-brands fa-whatsapp"></i></a>
+            <a href="tel:+917013796123" class="social-call"><i class="fa-solid fa-phone"></i></a>
             <a href="https://www.linkedin.com/in/saisatyakarthikeya" class="social-linked"><i class="fab fa-linkedin"></i></a>
             <a href="https://github.com/karthikeya03" class="social-github"><i class="fa-brands fa-github"></i></a>
         </div>
@@ -24,11 +24,15 @@ function addMainButtonEventListeners() {
         document.getElementById('main-content').innerHTML = `
             <div class="section">
                 <h2>Details</h2>
-                <p>Details about me...</p>
-                <p>Details about me...</p>
-                <p>Details about me...</p>
-                <p>Details about me...</p>
-                <p>Details about me...</p>
+                <p>As a Computer Science Engineering student, <br> I am dedicated to 
+                expanding my knowledge in new programming 
+                languages and exploring various technologies. <br><br>
+                Currently, I am focusing on objectively building my skills.
+                I have honed my expertise in web development and AWS Cloud development.
+                <br><br>
+                Additionally, I have acquired proficiency in programming 
+                languages such as C++ and Java.</p>
+                <br>
                 <button class="back-btn">Back</button>
             </div>
         `;
