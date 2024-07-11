7f1d223672b87cb0ad428348cc0b138ea48d329a
sai satya karthikeya <120096427+karthikeya03@users.noreply.github.com>
2024-07-11 09:48:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+document.getElementById('about-btn').addEventListener('click', function() {
+    document.getElementById('main-content').innerHTML = `
+        <div class="div1">
+            <div class="profile">
+                <img src="2026.png" alt="Profile Image">
+            </div>
+        </div>
+        <div class="div2">
+            <h1><span class="border">Sai Satya Karthikeya</span></h1>
+            <h3><span class="text">"computer science student"</span></h3>
+        </div>
+        <div class="social"> 
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=saisatyakarthikeya@gmail.com" class="social-mail"><i class="fa-solid fa-envelope"></i></a>
+            <a href="tel:+917013796123" class="social-call"><i class="fa-solid fa-phone"></i></a>
+            <a href="https://www.linkedin.com/in/saisatyakarthikeya" class="social-linked"><i class="fab fa-linkedin"></i></a>
+            <a href="https://github.com/karthikeya03" class="social-github"><i class="fa-brands fa-github"></i></a>
+        </div>
+    `;
+});
+
+document.getElementById('education-btn').addEventListener('click', function() {
+    document.getElementById('main-content').innerHTML = `
+        <div class="section">
+            <h2>Education</h2>
+            <p>Details about education...</p>
+            <p>Details about education...</p>
+            <p>Details about education...</p>
+            <p>Details about education...</p>
+            <p>Details about education...</p>
+           
+        </div>
+    `;
+});
+
+document.getElementById('contact-btn').addEventListener('click', function() {
+    document.getElementById('main-content').innerHTML = `
+        <div class="section">
+            <h2>Contact</h2>
+            <p>Details about contact...</p>
+        </div>
+    `;
+});
